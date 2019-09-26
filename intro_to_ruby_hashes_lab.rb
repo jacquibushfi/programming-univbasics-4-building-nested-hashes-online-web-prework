0185ec6608cb428395b9afe9106eadb99a867b0b
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-26 15:59:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,20 +31,19 @@ def monopoly_with_third_tier
     :names=>
     {
       :reading_railroad=>
-      {
+      {},
  
-      },
       :pennsylvania_railroad=>
       {
  
       },
       :b_and_o_railroad=>
       {
-        # "mortgage_value"=>"$400"
+       
       },
       :shortline=>
       {
-      #  "mortgage_value"=>"$800"
+     
       }
     }
   }
